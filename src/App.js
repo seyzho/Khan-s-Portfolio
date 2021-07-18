61f518fb196bc9b6b4193a943198fbd391ab6ebd
ShereKhan Adams <sherekhanadams@gmail.com>
2021-07-18 16:26:45 -0700

    redoing files from last repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, makeStyles } from '@material-ui/core';
+import Home from './components/Home';
+import Portfolios from './components/Portfolios';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/images/bg.jpeg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  }
+}))
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Home />
+      <About />
+      <Portfolios />
+      <Contact />
+      <Footer />
     </div>
   );
 }
